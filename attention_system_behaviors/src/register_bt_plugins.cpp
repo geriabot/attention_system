014cb0d83c40c527651e78fe4c2bb6ac2854fe13
dev-scobosb <dev.scobosb@gmail.com>
2026-06-12 12:58:23 +0200

    Deleted deprecated BT nodes and test executables

--- attention_system_behaviors/src/register_bt_plugins.cpp
+++ attention_system_behaviors/src/register_bt_plugins.cpp
@@ -1,10 +1,8 @@
 #include "behaviortree_cpp/bt_factory.h"
 
-#include "attention_system_behaviors/ActivateTwisting.hpp"
 #include "attention_system_behaviors/ChangeVisualDetectionClass.hpp"
 #include "attention_system_behaviors/ClearVisualDetectionClass.hpp"
 #include "attention_system_behaviors/DetectionExists.hpp"
-#include "attention_system_behaviors/DetectionNearSightCenter.hpp"
 #include "attention_system_behaviors/DetectionNearSightLimit.hpp"
 #include "attention_system_behaviors/GetDetections2DInformation.hpp"
 #include "attention_system_behaviors/GetTFCompactTreeInformation.hpp"
@@ -27,7 +25,6 @@ BT_REGISTER_NODES(factory)
   factory.registerNodeType<attention_system_behaviors::ChangeVisualDetectionClass>("ChangeVisualDetectionClass");
   factory.registerNodeType<attention_system_behaviors::ClearVisualDetectionClass>("ClearVisualDetectionClass");
   factory.registerNodeType<attention_system_behaviors::DetectionExists>("DetectionExists");
-  factory.registerNodeType<attention_system_behaviors::DetectionNearSightCenter>("DetectionNearSightCenter");
   factory.registerNodeType<attention_system_behaviors::DetectionNearSightLimit>("DetectionNearSightLimit");
   factory.registerNodeType<attention_system_behaviors::GetDetections2DInformation>("GetDetections2DInformation");
   factory.registerNodeType<attention_system_behaviors::GetTFCompactTreeInformation>("GetTFCompactTreeInformation");
@@ -40,7 +37,6 @@ BT_REGISTER_NODES(factory)
   factory.registerNodeType<attention_system_behaviors::RequestSingleDetectionTrackPoint>("RequestSingleDetectionTrackPoint");
   factory.registerNodeType<attention_system_behaviors::StartTrackTF>("StartTrackTF");
   factory.registerNodeType<attention_system_behaviors::TFExists>("TFExists");
-  factory.registerNodeType<attention_system_behaviors::ActivateTwisting>("ActivateTwisting");
   factory.registerNodeType<attention_system_behaviors::PublishTwist>("PublishTwist");
   factory.registerNodeType<attention_system_behaviors::RequestNDetectionsSameClassTrackPoint>("RequestNDetectionsSameClassTrackPoint");
   factory.registerNodeType<attention_system_behaviors::TriggerDeactivationService>("TriggerDeactivationService");
