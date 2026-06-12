281de100c60be38024153773dafd12cf08dcb0fc
dev-scobosb <dev.scobosb@gmail.com>
2026-06-12 13:29:46 +0200

    Changed mantainer references

--- llm_management/llm_router/llm_router/setup.py
+++ llm_management/llm_router/llm_router/setup.py
@@ -13,7 +13,7 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='scobosb',
+    maintainer='dev-scobosb',
     maintainer_email='dev.scobosb@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
